8c3b4875c08c91b6fccc2df2c20ddfdccb7654b1
Student <phone.asim@gmail.com>
2018-01-01 22:31:53 +1100

    2) SplashScreen

---
+++ app/src/main/java/com/karkun/ActivityLogo.java
@@ -0,0 +1,19 @@
+package com.karkun;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Windows on 1/01/2018.
+ */
+
+public class ActivityLogo extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
---
+++ app/src/main/java/com/karkun/SplashScreen/ActivitySplash.java
@@ -0,0 +1,23 @@
+package com.karkun.SplashScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.karkun.ActivityMain;
+
+/**
+ * Created by Windows on 1/01/2018.
+ */
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       Intent intent = new Intent(this, ActivityMain.class);
+       startActivity(intent);
+       finish();
+    }
+}
